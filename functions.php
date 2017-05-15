91b05af37b45c44c9d1b6c71a1c1af0ffb81dfe5
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-05-15 13:10:24 +0200

    Forcer activation ACF sur site

--- functions.php
+++ functions.php
@@ -159,3 +159,22 @@ return $creds;
 	$creds = 'Copyright [footer_copyright] | Céline Deflers';
 	return $creds;
 }
+
+
+//* Contrôle si Advanced Custom Field est actif sur le site
+if ( ! function_exists( 'get_field' ) ) {
+	// Notice dans le back-office au moment de la désactivation
+	add_action('admin_notices','gn_warning_admin_missing_acf');
+	function gn_warning_admin_missing_acf() {
+	   $plugin_url = get_bloginfo('url') . '/wp-admin/plugins.php';
+	   $output = '<div id="my-custom-warning" class="error fade">';
+	   $output .= sprintf('<p><strong>Attention</strong>, ce site ne fonctionne pas sans l\'extension <strong>Advanced Custom Fields</strong>. Merci d\'activer cette <a href="%s">extension</a>.</p>', $plugin_url);
+	   $output .= '</div>';
+	   echo $output;
+	 }
+	// Notice dans le front qui masque tout le contenu et affiche le lien pour ce connecter
+	add_action( 'template_redirect', 'gn_template_redirect_warning_missing_acf', 0 );
+	function gn_template_redirect_warning_missing_acf() {
+		wp_die( sprintf( 'Ce site ne fonctionne pas sans l\'extension <strong>Advanced Custom Fields</strong>. Merci de vous <a href="%s">connecter au site</a> pour l\'activer.', wp_login_url() ) );
+	}
+}
